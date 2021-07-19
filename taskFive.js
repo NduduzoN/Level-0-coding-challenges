897c31018cc872a0a8b259cacfde1cf3dbcbe797
Nduduzo Ndabandaba <giftnduduzo@gmail.com>
2021-07-19 20:43:56 +0200

    Added code for task 0.5

---
+++ taskFive.js
@@ -0,0 +1,7 @@
+function areaOfTriangle(sideOne ,sideTwo, sideThree){
+
+    semiperimeter = 0.5*(sideOne + sideTwo + sideThree);
+    area = Math.sqrt(semiperimeter * (semiperimeter-sideOne) * (semiperimeter-sideTwo) * (semiperimeter-sideThree));
+    
+return console.log(area);
+}
