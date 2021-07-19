07736e9a1941b660453bad331f60e83fe1512282
Nduduzo Ndabandaba <giftnduduzo@gmail.com>
2021-07-19 22:43:59 +0200

    Added code for task 0.7

---
+++ taskSeven.js
@@ -0,0 +1,11 @@
+function convertCelsiusToFahrenheit(temperature){
+
+    fahrenheit = (temperature*1.8) + 32;
+    return fahrenheit;
+}
+
+function convertFahrenheitToCelsius(temperature){
+
+    celsius = (temperature - 32) /1.8;
+    return celsius;
+}
