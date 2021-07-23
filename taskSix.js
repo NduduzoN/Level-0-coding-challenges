0b92d19db835536cc36e87e49474b637f0a73640
Nduduzo Ndabandaba <giftnduduzo@gmail.com>
2021-07-23 10:33:41 +0200

    Fixed the return on maximum function

--- taskSix.js
+++ taskSix.js
@@ -2,5 +2,5 @@ function maximum(){
 
     var args = Array.from(arguments);
     var sortedString = args.sort((a,b) => b - a);
-    return console.log(sortedString[0].toString());
+    return sortedString[0];
 }
