e950bb3cb8611e5a276760dcd83c601b96937e1f
Nduduzo Ndabandaba <giftnduduzo@gmail.com>
2021-07-23 10:38:33 +0200

    Fixed the return on vowel function

--- taskNine.js
+++ taskNine.js
@@ -5,11 +5,11 @@ var array = [];
 var matchedVowels;
 var regExpressionVowels = /[aeiou]/gi;
 
-if(regExpressionVowels.test(word) != true) return console.log("Word contains no vowels");
+if(regExpressionVowels.test(word) != true) return "Word contains no vowels";
 matchedVowels = word.match(regExpressionVowels);
 
 var removeDuplicated = new Set(matchedVowels);
 array = Array.from(removeDuplicated);
-return console.log("Vowels : "+array.join(" "));
+return "Vowels : "+array.join(" ");
 }
 
